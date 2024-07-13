372e5a6b98bf01ee077b91edc0567cd2ba603e27
vishalj <vishalj2805@gmail.com>
2024-07-13 14:50:44 +0530

    Minor Changes

--- src/test/java/org/study/tests/LoginTest.java
+++ src/test/java/org/study/tests/LoginTest.java
@@ -2,6 +2,7 @@ package org.study.tests;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import org.openqa.selenium.WebDriver;
 import org.study.pageobjects.LandingPage;
 import org.study.testcomponents.ReportsConfig;
 import org.study.testcomponents.TestBase;
@@ -10,6 +11,7 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class LoginTest extends TestBase {
+    
 
     @Test
     public void login() throws IOException {
@@ -17,8 +19,4 @@ public class LoginTest extends TestBase {
         LandingPage landingPage = new LandingPage(driver);
         landingPage.login("vishal.jadhav.2805@gmail.com", "Vishalj2805");
     }
-
-
-
-
 }
--- src/test/java/org/study/tests/TestTempTest.java
+++ src/test/java/org/study/tests/TestTempTest.java
@@ -1,24 +1,28 @@
 package org.study.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import io.cucumber.java.hu.Ha;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class TestTempTest {
 
     public static void main(String[] args) throws InterruptedException {
-        int x=1002;
-        System.out.println(x%10);
-        System.out.println(x/10);
+        int[] num = {1,1,1,3,3,2,2,4,4,5,5,6,6,9,7,7,8,8};
+
+        int unique = 0;
+
+        for (int i=0;i<num.length;i++){
+            int currentNum = num[i];
+            int count= 0;
+            for (int j=0;j<num.length;j++){
+                if (num[i]==num[j]){
+                    count++;
+                }
+            }
+            if (count==1){
+                unique=num[i];
+            }
+        }
+        System.out.println(unique);
     }
 }
