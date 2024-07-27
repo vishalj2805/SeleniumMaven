c9bebe91647abfac27219f36032fe9f499cfb486
vishalj2805 <vishalj2805@gmail.com>
2024-07-27 15:24:33 +0530

    Robot Class

--- src/test/java/org/study/tests/TestTempTest.java
+++ src/test/java/org/study/tests/TestTempTest.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.time.Duration;
 import java.util.HashMap;
@@ -15,25 +18,25 @@ import java.util.HashMap;
 
 public class TestTempTest {
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException, IOException, AWTException {
 
-        HashMap<String, Object> downloadPathDirChange = new HashMap<>();
-        downloadPathDirChange.put("profile.default_content_settings.popups", 0);
-        downloadPathDirChange.put("download.default_directory", System.getProperty("user.dir"));
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setExperimentalOption("prefs", downloadPathDirChange);
+        StringSelection filePath = new StringSelection("D:\\Resume\\Vishal Jadhav QA Automation Resume.docx");
+        Robot robot = new Robot();
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(filePath, null);
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.get("https://www.ilovepdf.com/word_to_pdf");
         driver.findElement(By.xpath("//a[@id='pickfiles']")).click();
         Thread.sleep(3000);
-        Runtime.getRuntime().exec("fileupload.exe");
-
-
-
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
 
         Thread.sleep(3000);
         driver.quit();
